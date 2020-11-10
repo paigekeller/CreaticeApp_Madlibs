4e071eab15ecb50116d389ab5c29e410dee8293c
Paige Keller <paige.keller22@d155.org>
2020-11-10 14:33:54 -0600

    Initial Commit

---
+++ _CreativeApps_called Mod4_CreativeApp_MadlibsKellerP/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  _CreativeApps_called Mod4_CreativeApp_MadlibsKellerP
+//
+//  Created by Tiger Coder on 11/10/20.
+//  Copyright © 2020 clc.Paige. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
